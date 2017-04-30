609538376fa9fd1391465b04c210141feb24cd27
Omri Webber <845868822@kent-202-10m.local>
2017-05-01 11:48:40 +1200

    working on menus

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Slideshow
 var slideIndex = 0;
 carousel();
 
@@ -12,3 +13,83 @@ function carousel() {
     x[slideIndex-1].style.display = "block";
     setTimeout(carousel, 5000);
 }
+
+$(document).ready(function(){
+	// variables
+	var itemExpanded = $(".expanded");
+	var expandButton = $(".expandButton");
+  var coffeeButton = $(".coffeeButton");
+  var coffeeTable = $(".coffeeTable");
+
+
+
+	//Expanded Items show/hide
+	expandButton.click(function(){
+		if (itemExpanded.is(":visible")) {
+			itemExpanded.fadeOut('1000');
+			itemExpanded.hide('slow');
+		}
+		else {
+		  itemExpanded.fadeIn('1000');
+			itemExpanded.show();
+		}
+	});
+
+  //Coffee Menu show/hide
+	coffeeButton.click(function(){
+		if (coffeeTable.is(":visible")) {
+			coffeeTable.fadeOut('1000');
+			coffeeTable.hide('slow');
+		}
+		else {
+		  coffeeTable.fadeIn('1000');
+			coffeeTable.show();
+		}
+	});
+
+  //Baking Menu show/hide
+	bakingButton.click(function(){
+		if (bakingTable.is(":visible")) {
+			bakingTable.fadeOut('1000');
+			bakingTable.hide('slow');
+		}
+		else {
+		  bakingTable.fadeIn('1000');
+			bakingTable.show();
+		}
+	});
+
+  //Breakfast Menu show/hide
+	Button.click(function(){
+		if (breakfastTable.is(":visible")) {
+			breakfastTable.fadeOut('1000');
+			breakfastTable.hide('slow');
+		}
+		else {
+		  breakfastTable.fadeIn('1000');
+			breakfastTable.show();
+		}
+	});
+
+  //Coffee show/hide
+	lunchButton.click(function(){
+		if (lunchTable.is(":visible")) {
+			lunchTable.fadeOut('1000');
+			lunchTable.hide('slow');
+		}
+		else {
+		  lunchTable.fadeIn('1000');
+			lunchTable.show();
+		}
+	});
+
+});
+
+
+// Smooth Scrolling
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
